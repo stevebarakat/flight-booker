37c72bb09fb8b40f61202dd7a68f931be454b8b4
stevebarakat <stevebarakat@gmail.com>
2024-05-08 00:28:28 -0400

    test 2

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -8,10 +8,29 @@ test("has title", async ({ page }) => {
   await expect(page).toHaveTitle(/Flight Booker/);
 });
 
-test("get started link", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
+test("book button is enabled when depart date is today", async ({ page }) => {
+  function generateDate(days: number) {
+    return new Date(Date.now() + days * 86400000).toISOString().split("T")[0];
+  }
+  const TODAY = generateDate(0);
+  await page.goto("/");
   const departDate = page.getByLabel("Depart Date");
-  await departDate.fill("2022-01-01");
+  await departDate.fill(TODAY);
+
+  const bookButton = page.getByRole("button");
+  expect(await bookButton.isEnabled()).toBe(true);
+});
+
+test("book button is disabled when depart date is in the past", async ({
+  page,
+}) => {
+  function generateDate(days: number) {
+    return new Date(Date.now() + days * 86400000).toISOString().split("T")[0];
+  }
+  const LAST_WEEK = generateDate(-7);
+  await page.goto("/");
+  const departDate = page.getByLabel("Depart Date");
+  await departDate.fill(LAST_WEEK);
 
   const bookButton = page.getByRole("button");
   expect(await bookButton.isEnabled()).toBe(false);
--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:5173",
+    baseURL: "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -37,41 +37,31 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
 
     /* Test against mobile viewports. */
     // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
+    //   name: "Mobile Chrome",
+    //   use: { ...devices["Pixel 5"] },
     // },
     // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    //   name: "Mobile Safari",
+    //   use: { ...devices["iPhone 12"] },
     // },
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run dev",
+    url: "http://localhost:5173/",
+    reuseExistingServer: !process.env.CI,
+  },
 });
